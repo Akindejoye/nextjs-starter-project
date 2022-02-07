f6be150e549a06e49e2752a835a54a804dff4a39
Akindejoye <tydruhill@gmail.com>
2022-02-07 02:48:16 +0100

    Setting up basic pages and Layout

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://images.pexels.com/photos/2676589/pexels-photo-2676589.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        address: 'Some address 5, 12345 Some City',
+        description: 'This is a first meetup!'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://images.pexels.com/photos/879537/pexels-photo-879537.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+        address: 'Some address 5, 123456 Some City',
+        description: 'This is a second meetup!'
+    }
+];
+
+const HomePage = () => {
+    return(
+        <MeetupList meetups={DUMMY_MEETUPS} />
+    )
+}
+
+export default HomePage;
